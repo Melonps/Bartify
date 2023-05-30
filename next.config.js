f4081e6b3d6716ce8df44f41759a9a21103fca1e
Melops <95263124+Melonps@users.noreply.github.com>
2023-05-30 21:00:58 +0900

    [INIT] init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      poll: 800,
+      aggregateTimeout: 300,
+    }
+    return config
+  },
+}
