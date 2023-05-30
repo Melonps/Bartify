0449da97fa2403d9cc7eb995bc0f3ff966ef2891
Melops <95263124+Melonps@users.noreply.github.com>
2023-05-30 21:31:45 +0900

    [FIX] グラフの描画に成功 #1
    props当は入力していませんが、plotlyを使うことが出来ました

--- libs/DrawGraph.tsx
+++ libs/DrawGraph.tsx
@@ -1,3 +1,26 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
-import {Layout, PlotData} from "plotly.js";
+import { Layout, PlotData } from "plotly.js";
+
+type Props = {}
+
+const Graph: React.FC<Props> = () => {
+
+    const data1:Partial<PlotData> = {
+        type: 'bar',
+        x: ['giraffes', 'orangutans', 'monkeys'],
+        y: [20, 14, 23],
+        name: 'SF Zoo',
+    }
+    const layout1:Partial<Layout> = { title: '３次元グラフ'};
+    // 下にある<Plot data = {}> のdataの型は Partial<PlotData>[]
+    // サンプルとしてわかりやすいように型を書いています
+    const allData:Partial<PlotData>[] = [data1]
+    
+    return (
+        <Plot data={allData} layout={layout1} />
+    );
+
+}
+
+export default Graph;
